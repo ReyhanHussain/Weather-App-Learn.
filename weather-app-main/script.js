37f0537648d5ad760d61691da8b8f75f347eef58
reyhan <reyhanhussain@hotmail.com>
2026-07-31 21:22:52 +0530

    added searching feature yet to impliment it

--- weather-app-main/script.js
+++ weather-app-main/script.js
@@ -1,8 +1,8 @@
 const hiddenUnitsMenu = document.querySelector('.unitsOptionsMenu');
 const units = document.querySelector('.units');
 const searcBar = document.querySelector('#searcBar');
-
-
+const searchLoading = document.querySelector('.searchLoading');
+const searchInProgress = document.querySelector('.searchInProgress');
 async function fetchWeather(latitude, longitude) {
     try {
         const a = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=temperature_2m_max,temperature_2m_min,weather_code&hourly=temperature_2m,weather_code&current=temperature_2m,weather_code,apparent_temperature,relative_humidity_2m,wind_speed_10m,precipitation`);
@@ -26,29 +26,97 @@ async function fetchGetCordinates(name) {
         console.log(e);
     }
 }
+function addSearchInProgressInfographics(infoToShow) {
+    const element = searchInProgress.cloneNode(true);
+    element.innerText = infoToShow;
+    element.classList.remove('hidden')
+    searchLoading.prepend(element);
+}
+function removeAllSearchSuggestions() {
+    searchLoading.replaceChildren()
+}
 
-searcBar.addEventListener('input', (e)=>{
+function addSearchSuggestion(apiResponse) {
+    if (apiResponse.results) {
+        let resultsArray = [...(apiResponse.results)]
 
+        resultsArray.forEach((a) => {
+            const name = a.name ?? "";
+            const state = a.admin1 ?? "";
+            const country = a.country ?? "";
+            const latitude = a.latitude ?? "";
+            const longitude = a.longitude ?? "";
+
+            const element = searchInProgress.cloneNode(true);
+            element.classList.remove('hidden')
+            element.innerText = `${name} ,${state} ,${country}`;
+            element.dataset.latitude = latitude
+            element.dataset.longitude = longitude
+
+            searchLoading.prepend(element);
+            console.log(element);
+        })
+    }
+    else {
+        addSearchInProgressInfographics('Not found.....');
+    }
+}
+
+
+async function replaceWeather(latitude, longitude){
+    const response = await fetchWeather(latitude, longitude);
+    return
+}
+searcBar.addEventListener('input', async (e) => {
+    console.log(e.target.value);
+    let searchedPlace = e.target.value;
+    try {
+        addSearchInProgressInfographics('⌛︎ Search in progress...');
+        let apiResponse = await fetchGetCordinates(searchedPlace);
+        console.log(apiResponse);
+        removeAllSearchSuggestions();
+        await addSearchSuggestion(apiResponse);
+    }
+
+    catch (e) {
+        console.log(e);
+    }
 })
+searcBar.addEventListener('click', (e)=>{
+    searchLoading.classList.remove('hidden');
+    console.log(e);
+});
 
+searchLoading.addEventListener('click', (e) => {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    const button = e.target.closest(".searchInProgress");
+    if (!button) {
+        return;
+    }
+    const latitude = button.dataset.latitude;
+    const longitude = button.dataset.longitude;
+    button.classList.add('selectedElement');
+    
+    replaceWeather(latitude, longitude)
+    searchLoading.replaceChildren();
+    searchLoading.classList.add('hidden')
+    console.log(e);
+})
 
 
 units.addEventListener('click', () => {
     console.log('options operned')
     hiddenUnitsMenu.classList.toggle('hidden')
+})
+
+window.addEventListener('click', (e)=>{
+const click = e.target.closest(".searchLoading");
+const click2 = e.target.closest('#searcBar');
+    if(click || click2){
+        return;
+    }
+    else{
+    removeAllSearchSuggestions()
+    searchLoading.classList.add('hidden');
+    }
 })
